a2d73b6c32b131a81b960938416b223f814415b4
prdalmia <pdalmia@wisc.edu>
2020-06-01 11:55:36 -0500

    message

--- apps/bfs/kernel.cu
+++ apps/bfs/kernel.cu
@@ -620,6 +620,7 @@ void gg_main_pipe_1_wrapper(CSRGraph& gg, int& LEVEL, PipeContextT<Worklist2>& p
     cudaEvent_t stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
+    cudaEventRecord(start);
     // gg_main_pipe_1_gpu<<<1,1>>>(gg,LEVEL,pipe,blocks,threads,cl_LEVEL, enable_lb);
     gg_main_pipe_1_gpu_gb<<<60, __tb_gg_main_pipe_1_gpu_gb>>>(gg,LEVEL,pipe,cl_LEVEL, enable_lb, gg_main_pipe_1_gpu_gb_barrier, global_sense, perSMsense, done, global_count, local_count, last_block, NUM_SM);
     cudaEventRecord(stop);
