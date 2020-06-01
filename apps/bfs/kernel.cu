11a53fbe9b03ef1308420c40139d9f7c160716a0
prdalmia <pdalmia@wisc.edu>
2020-06-01 11:59:35 -0500

    message

--- apps/bfs/kernel.cu
+++ apps/bfs/kernel.cu
@@ -213,6 +213,7 @@ int numTBs_perSM = (int)ceil((float)gridDim.x / numBlocksAtBarr);
 if(isMasterThread){
   printf("chalo barrier shuru kare for %d", blockIdx.x);
 }
+ __syncthreads();
 joinBarrier_helperSRB(global_sense, perSMsense, done, global_count, local_count, last_block,
 numBlocksAtBarr, smID, perSM_blockID, numTBs_perSM,
 isMasterThread);
