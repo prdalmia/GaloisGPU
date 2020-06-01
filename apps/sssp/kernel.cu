f73f53d2f1438ec347080653641effe72b764fbd
prdalmia <pdalmia@wisc.edu>
2020-05-31 23:31:49 -0500

    message

--- apps/sssp/kernel.cu
+++ apps/sssp/kernel.cu
@@ -42,7 +42,6 @@ if (atomicCAS(globalBarr, numBarr, 0) == numBarr) {
 // atomicCAS acts as a load acquire, need TF to enforce ordering
 __threadfence();
 *global_sense = *sense;
-  printf("Setting global sense = sense \n");
 }
 else { // increase backoff to avoid repeatedly hammering global barrier
 // (capped) exponential backoff
