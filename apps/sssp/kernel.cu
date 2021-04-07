c18ca4d9dcea72b8f73599e0a5e7ddc8845c3630
prdalmia <pdalmia@wisc.edu>
2021-04-07 13:11:52 -0500

    Fixing call to Naive

--- apps/sssp/kernel.cu
+++ apps/sssp/kernel.cu
@@ -746,7 +746,7 @@ void gg_main_pipe_1_wrapper(CSRGraph& gg, gint_p glevel, int& curdelta, int& i, 
     unsigned int* global_count;
     unsigned int* local_count; 
     unsigned int *last_block;
-    bool naive = false;
+    bool naive = true;
     int NUM_SM = ggc_get_nSM();
     cudaMallocManaged((void **)&global_sense,sizeof(bool));
     cudaMallocManaged((void **)&done,sizeof(bool));
